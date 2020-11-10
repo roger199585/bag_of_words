0cd05367e5fde04ca04203a9e7574fe0dcc53a6e
Corn <roger199585@yahoo.com.tw>
2020-11-10 21:10:07 +0800

    Update: finial training process

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-ROOT="/home/dinosaur/refactor/bag_of_words"
+import os 
+
+ROOT = os.environ.get('ROOT', '/home/dinosaur/refactor/bag_of_words')
+RESULT_PATH = os.environ.get('RESULT_PATH', '../tensorboard')
