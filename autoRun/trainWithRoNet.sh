b20f39cf5bcbb2fb9e5383cbbedf4e3b3b724d4a
Corn <roger199585@yahoo.com.tw>
2021-01-25 14:55:28 +0800

    RoNet train on 136

--- autoRun/trainWithRoNet.sh
+++ autoRun/trainWithRoNet.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data bottle --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data cable --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-wait
-CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data capsule --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data carpet --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-wait
-CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data grid --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data hazelnut --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-wait
-CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data leather --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data metal_nut --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
-wait
+# CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data bottle --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data cable --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# wait
+# CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data capsule --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data carpet --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# wait
+# CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data grid --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data hazelnut --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# wait
+# CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data leather --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data metal_nut --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
+# wait
 CUDA_VISIBLE_DEVICES=0,1 python model_RoNet_weightSample.py --data pill --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
 CUDA_VISIBLE_DEVICES=2,3 python model_RoNet_weightSample.py --data screw --kmeans 128 --type good --train_batch 16 --with_mask True --patch_size 64 --epoch 40 &
 wait
