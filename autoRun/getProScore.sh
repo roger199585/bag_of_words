0e82beed66d6245189afd0ebf36a6242661268b6
Corn <roger199585@yahoo.com.tw>
2021-01-31 11:17:53 +0800

    Change on Variola

--- autoRun/getProScore.sh
+++ autoRun/getProScore.sh
@@ -1,10 +1,14 @@
 cd $PWD
 
-CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data pill --index 38 &
-CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data screw --index 32 &
-CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data tile --index 6 &
-CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data toothbrush --index 29 &
-CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data transistor --index 17 &
-CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data wood --index 13 &
-CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data zipper --index 2 &
+# CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data pill --index 38 &
+# CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data screw --index 32 &
+# CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data tile --index 6 &
+# CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data toothbrush --index 29 &
+# CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data transistor --index 17 &
+# CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data wood --index 13 &
+# CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data zipper --index 2 &
+
+# draw Multi Map
+CUDA_VISIBLE_DEVICES=0,1 python evalRoNet/PRO.py --kmeans 128 --data capsule --index 33 &
+CUDA_VISIBLE_DEVICES=2,3 python evalRoNet/PRO.py --kmeans 128 --data hazelnut --index 23 &
 wait
